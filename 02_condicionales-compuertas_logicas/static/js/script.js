61282e853c16a21553fe6daf1956591feafb5488
Arael_A2026 <araelanabalon@liceovvh.cl>
2026-03-05 15:15:09 -0300

    condicionales

---
+++ 02_condicionales-compuertas_logicas/static/js/script.js
@@ -0,0 +1,36 @@
+console.log("Conectado");
+
+// 1. El Portero Digital
+function porteroDiital() {
+    let edad = parseInt(prompt("Ingrese su edad: "));
+    if (edad >= 18) {
+        alert("Acceso permitido a la App");
+    } else if (edad < 18) {
+        alert("Acceso denegado: necesitas ser mayor de edad");
+    } else {
+        alert("Error: El nombre no puede estar en blanco")
+    }
+}
+
+// 2. Validador de Nombres
+function validarNombre() {
+    let nombre = prompt("Ingresa tu nombre: ");
+    let usuarios = [""];
+    if (nombre.length < 1) {
+        
+    } else {
+        
+    }
+}
+
+// 3. El Filtro de Duplicados
+function filtroDuplicados() {
+    let productos = ["celular", "teclado", "mouse"];
+    let nuevoProducto = parseFloat(prompt("Ingresa un producto: "));
+    if (productos.includes(nuevoProducto)) {
+        productos.push(nuevoProducto);
+        alert(`Inventario actualizado: \n${productos.join(", ")}`);
+    } else {
+        alert("El producto ya está en el inventario");
+    }
+}
